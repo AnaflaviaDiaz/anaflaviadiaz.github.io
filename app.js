95727f37b4521e83481cdc456252b63ad4750c27
anaflaviadiaz <anaflaviadiazmartel5a@gmail.com>
2018-09-21 17:48:19 -0500

    advance

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ const demoFoodmap = 'https://anaflaviadiaz.github.io/lim-2018-01-foodmap/';
 const demoVisitor = 'https://anaflaviadiaz.github.io/wework/src/';
 const demoEdutic = 'https://anaflaviadiaz.github.io/lim-2018-01-social-network-frameworks/my-app/login';
 
+const linkLinkedln = 'https://www.linkedin.com/in/anaflavia-diaz/';
+const linkGithub = 'https://github.com/AnaflaviaDiaz';
+const linkCV = 'https://drive.google.com/file/d/1RZeXPyFhJYbmDWBR6km_6FYPbvzHrsZj/view';
+
 const btnRepoSalutem = document.getElementById('btn-repo-salutem');
 const btnRepoFoodmap = document.getElementById('btn-repo-foodmap');
 const btnRepoVisitor = document.getElementById('btn-repo-visitor');
@@ -35,9 +39,18 @@ const btnDemoFoodmap = document.getElementById('btn-demo-foodmap');
 const btnDemoVisitor = document.getElementById('btn-demo-visitor');
 const btnDemoEdutic = document.getElementById('btn-demo-edutic');
 
+const btnLinkedln = document.getElementById('linkedin');
+const btnGithub = document.getElementById('github');
+const btnCV = document.getElementById('cv');
+const btnEmail = document.getElementById('gmail');
+
 const openPage = (route) => {
   window.open(route, '_blank');
 };
+
+const sendEmail = () => {
+  
+}
 
 btnRepoSalutem.addEventListener('click', () => openPage(repoSalutem));
 btnRepoFoodmap.addEventListener('click', () => openPage(repoFoodmap));
@@ -48,6 +61,11 @@ btnDemoSalutem.addEventListener('click', () => openPage(demoSalutem));
 btnDemoFoodmap.addEventListener('click', () => openPage(demoFoodmap));
 btnDemoVisitor.addEventListener('click', () => openPage(demoVisitor));
 btnDemoEdutic.addEventListener('click', () => openPage(demoEdutic));
+
+btnLinkedln.addEventListener('click', () => openPage(linkLinkedln));
+btnGithub.addEventListener('click', () => openPage(linkGithub));
+btnCV.addEventListener('click', () => openPage(linkCV));
+// btnEmail.addEventListener('click', () => openPage(linkLinkedln));
 
 document.addEventListener('DOMContentLoaded', function () {
   const elems = document.querySelectorAll('.sidenav');
